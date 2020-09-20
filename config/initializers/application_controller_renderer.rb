359657b0cf1960deb38bc7a31a113d32b8d53368
EC2 Default User <kapibara.bba0924@gmail.com>
2020-09-20 06:06:46 +0000

    [Add]meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+ 
+ Refile.secret_key = 'ab8ef591c9f36c1de0e90610d8161ba51a26f72f3f332ee1e911ce7bbd32e88f93b110d6033af5207ed0dee9a69a419a606f85ef38b424f67444579f04593fb1'
+ 
